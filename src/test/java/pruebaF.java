9a2cd627d721282bc117c2c6e509d5113b737f59
Rocael <rocaedua@gmail.com>
2018-03-03 22:08:23 -0600

    Prueba funcional

---
+++ src/test/java/pruebaF.java
@@ -0,0 +1,31 @@
+package Pruebas;
+
+import com.eviware.soapui.tools.SoapUITestCaseRunner;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Rocael Isidro
+ */
+public class pruebaF {
+    
+     
+ public pruebaF() {
+ }
+  @Test
+	public void shouldExecuteAllTestCases() throws Exception{
+		// given
+		SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
+             //String path="C:\\Users\\Pamela Palacios\\Documents\\farmacia-soapui-project.xml";
+              String path="/home/farmacia-soapui-project.xml";
+             runner.setProjectFile(path);
+     	//runner.setProjectProperties(properties);
+     	runner.run();
+	}
+
+}
